437c2fb252c494296e99538153898c9e4da6aa6b
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-06-14 23:17:29 +0200

    powiadomienia o taskach

--- app/src/main/java/net/bartor/todolist/MainActivity.kt
+++ app/src/main/java/net/bartor/todolist/MainActivity.kt
@@ -2,23 +2,29 @@ package net.bartor.todolist
 
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.Observer
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import net.bartor.todolist.db.AppDatabase
 import net.bartor.todolist.db.Task
+import net.bartor.todolist.notifications.NotificationService
 import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
     private lateinit var taskList: LiveData<List<Task>>
     private val adapterList: ArrayList<Task> = arrayListOf()
     private lateinit var db: AppDatabase
+    private var lastService: Intent? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +45,12 @@ class MainActivity : AppCompatActivity() {
             println(it)
             adapterList.clear()
             adapterList.addAll(it!!)
+
+            if (lastService != null) stopService(lastService)
+            lastService = Intent(this@MainActivity, NotificationService::class.java)
+            lastService!!.putExtra("tasks", adapterList)
+            startService(lastService!!)
+
             recycler.adapter.notifyDataSetChanged()
         })
 
